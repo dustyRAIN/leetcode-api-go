25b12be8138a5f5d0c36fc1d2106c80b8334b7cf
Guru Ananda <guru.ananda.aurora@gmail.com>
2023-03-11 19:09:18 +0600

    add integration tests for discussion module

--- leetcodeapi/z_integration_test.go
+++ leetcodeapi/z_integration_test.go
@@ -110,3 +110,154 @@ func (s *contestsSuite) TestGetContestRanking() {
 	s.Assert().Nil(err)
 	s.Assert().Equal(expected, result)
 }
+
+type discussionsSuite struct {
+	suite.Suite
+}
+
+func TestDiscussionsSuite(t *testing.T) {
+	suite.Run(t, &discussionsSuite{})
+}
+
+func (s *discussionsSuite) SetupTest() {
+	err := os.Setenv("LEETCODEAPI_ENV", "test")
+	s.Assert().Nil(err)
+}
+
+func (s *discussionsSuite) TearDownTest() {
+	err := os.Unsetenv("LEETCODEAPI_ENV")
+	s.Assert().Nil(err)
+}
+
+func (s *discussionsSuite) TestGetDiscussions() {
+	responseBody := []byte(`{
+		"data": {
+			"categoryTopicList": {
+				"edges": [{
+					"cursor": "cursor",
+					"node": {
+						"commentCount": 12,
+						"id": "id",
+						"pinned": false
+					}
+				}],
+				"totalNum": 1
+			}
+		}
+	}`)
+	expected := leetcodeapi.DiscussionList{
+		Data: []leetcodeapi.DiscussionListItem{
+			{
+				Cursor: "cursor",
+				Node: leetcodeapi.DiscussionNode{
+					CommentCount: 12,
+					Id:           "id",
+					Pinned:       false,
+				},
+			},
+		},
+		TotalNum: 1,
+	}
+
+	server := leetcodeapi.GetMockedHttpServer(responseBody, 200)
+	defer server.Close()
+
+	result, err := leetcodeapi.GetDiscussions([]string{}, []string{}, "", "", 0)
+
+	s.Assert().Nil(err)
+	s.Assert().Equal(expected, result)
+}
+
+func (s *discussionsSuite) TestGetDiscussion() {
+	responseBody := []byte(`{
+		"data": {
+			"topic": {
+				"hideFromTrending": false,
+				"id": 321321,
+				"pinned": false,
+				"subscribed": false,
+				"title": "title",
+				"viewCount": 32
+			}
+		}
+	}`)
+
+	expected := leetcodeapi.Discussion{
+		HideFromTrending: false,
+		Id:               321321,
+		Pinned:           false,
+		Subscribed:       false,
+		Title:            "title",
+		ViewCount:        32,
+	}
+
+	server := leetcodeapi.GetMockedHttpServer(responseBody, 200)
+	defer server.Close()
+
+	result, err := leetcodeapi.GetDiscussion(10)
+
+	s.Assert().Nil(err)
+	s.Assert().Equal(expected, result)
+}
+
+func (s *discussionsSuite) TestGetDiscussionComments() {
+	responseBody := []byte(`{
+		"data": {
+			"topicComments": {
+				"data": [{
+					"id": 123123,
+					"pinned": false,
+					"post": {
+						"content": "this is a comment"
+					}
+				}]
+			}
+		}
+	}`)
+
+	expected := []leetcodeapi.Comment{{
+		Id:     123123,
+		Pinned: false,
+		Post: leetcodeapi.Post{
+			Content: "this is a comment",
+		},
+	}}
+
+	server := leetcodeapi.GetMockedHttpServer(responseBody, 200)
+	defer server.Close()
+
+	result, err := leetcodeapi.GetDiscussionComments(10, "", 1, 1)
+
+	s.Assert().Nil(err)
+	s.Assert().Equal(expected, result)
+}
+
+func (s *discussionsSuite) TestGetCommentReplies() {
+	responseBody := []byte(`{
+		"data": {
+			"commentReplies": [{
+				"id": 123123,
+				"pinned": false,
+				"post": {
+					"content": "this is a comment"
+				}
+			}]
+		}
+	}`)
+
+	expected := []leetcodeapi.Comment{{
+		Id:     123123,
+		Pinned: false,
+		Post: leetcodeapi.Post{
+			Content: "this is a comment",
+		},
+	}}
+
+	server := leetcodeapi.GetMockedHttpServer(responseBody, 200)
+	defer server.Close()
+
+	result, err := leetcodeapi.GetCommentReplies(10)
+
+	s.Assert().Nil(err)
+	s.Assert().Equal(expected, result)
+}
